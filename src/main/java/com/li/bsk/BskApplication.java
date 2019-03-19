8b4eebecd33cf82f573598e81dd0a2a89a15b778
root <12345678>
2019-03-19 22:08:26 +0800

    Initial commit

---
+++ src/main/java/com/li/bsk/BskApplication.java
@@ -0,0 +1,13 @@
+package com.li.bsk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run (BskApplication.class, args);
+    }
+
+}
